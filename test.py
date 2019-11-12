7ff39a6772f64a7e26f18d14554755ce579e5a1e
Lindvar Legran <llag@equinor.com>
2019-11-12 07:41:58 +0100

    Test 2

--- test.py
+++ test.py
@@ -13,17 +13,19 @@ def get_json(url):
 
 
 if __name__ == '__main__':
-    for i in range(0, 500):
+    #url = 'https://ml-api-ml-production.playground.radix.equinor.com/'
+    url = 'http://localhost:5000/'
+    for i in range(0, 20):
         print(f'*************{i}******************')
-        #print(get_json('http://localhost:5000/api/testok/'))
-        #print(get_json('http://localhost:5000/api/test1/nothread'))
-        #print(get_json('http://localhost:5000/api/test1/thread'))
-        #print(get_json('http://localhost:5000/api/test2/nothread'))
-        #print(get_json('http://localhost:5000/api/test2/thread'))
-        #print(get_json('http://localhost:5000/api/test3/'))
-        #print(get_json('http://localhost:5000/api/test4/nothread'))
-        #print(get_json('http://localhost:5000/api/test4/thread'))
-        #print(get_json('http://localhost:5000/api/test5/'))
-        #print(get_json('http://localhost:5000/api/test6/nothread'))
-        #print(get_json('http://localhost:5000/api/test6/thread'))
-        print(get_json('http://localhost:5000/api/test7/thread'))
+        print(get_json(url+'api/testok/'))
+        print(get_json(url+'api/test1/nothread'))
+        print(get_json(url+'api/test1/thread'))
+        print(get_json(url+'api/test2/nothread'))
+        print(get_json(url+'api/test2/thread'))
+        print(get_json(url+'api/test3/'))
+        print(get_json(url+'api/test4/nothread'))
+        print(get_json(url+'api/test4/thread'))
+        print(get_json(url+'api/test5/'))
+        print(get_json(url+'api/test6/nothread'))
+        print(get_json(url+'api/test6/thread'))
+        print(get_json(url+'api/test7/thread'))
